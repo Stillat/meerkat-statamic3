2d6371430f0615996793c1367c06d5cbee642693
John Koster <john@stillat.com>
2020-07-05 12:59:23 -0500

    Adds a helper trait to easily resolve addon configuration

---
+++ src/Concerns/UsesConfig.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Stillat\Meerkat\Concerns;
+
+use Stillat\Meerkat\Meerkat;
+
+/**
+ * Trait UsesConfig
+ *
+ * Provides users with the ability to quickly resolve addon configuration entries.
+ *
+ * @package Stillat\Meerkat\Concerns
+ */
+trait UsesConfig
+{
+
+    /**
+     * Gets an addon configuration entry.
+     *
+     * @param $key
+     * @param null $default
+     * @return \Illuminate\Config\Repository|\Illuminate\Contracts\Foundation\Application|mixed
+     */
+    protected function getConfig($key, $default = null)
+    {
+        // Create a namespaced configuration key using "dot" notation.
+        $namespacedKey = Meerkat::CODE_ADDON_NAME.'.'.$key;
+
+        return config($namespacedKey, $default);
+    }
+
+}

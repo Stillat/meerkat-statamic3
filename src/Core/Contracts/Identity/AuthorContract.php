b44c62406b4787039126f6fe5ba26a0b61b09440
John Koster <john@stillat.com>
2020-07-11 15:59:54 -0500

    Basic transient implementation

--- src/Core/Contracts/Identity/AuthorContract.php
+++ src/Core/Contracts/Identity/AuthorContract.php
@@ -38,6 +38,7 @@ interface AuthorContract extends DataObjectContract, Serializable
     const KEY_USER_AGENT = 'user_agent';
     const KEY_EMAIL_ADDRESS = 'email';
     const KEY_NAME = 'name';
+    const AUTHENTICATED_USER_ID = 'authenticated_user_id';
 
     /**
      * Returns a value indicating if the author context is
--- src/Identity/StatamicAuthorFactory.php
+++ src/Identity/StatamicAuthorFactory.php
@@ -3,6 +3,7 @@
 namespace Stillat\Meerkat\Identity;
 
 use Statamic\Auth\User;
+use Statamic\Auth\UserProvider;
 use Stillat\Meerkat\Core\Contracts\Identity\AuthorContract;
 use Stillat\Meerkat\Core\Contracts\Identity\AuthorFactoryContract;
 
@@ -16,6 +17,18 @@ use Stillat\Meerkat\Core\Contracts\Identity\AuthorFactoryContract;
  */
 class StatamicAuthorFactory implements AuthorFactoryContract
 {
+
+    /**
+     * The Statamic UserProvider instance.
+     *
+     * @var UserProvider
+     */
+    private $statamicUserProvider = null;
+
+    public function __construct(UserProvider $userProvider)
+    {
+        $this->statamicUserProvider = $userProvider;
+    }
 
     /**
      * Constructs a valid AuthorContract instance from the prototype.
@@ -36,9 +49,50 @@ class StatamicAuthorFactory implements AuthorFactoryContract
         return null;
     }
 
+    /**
+     * Creates a Meerkat Core identity from the provided prototype information.
+     *
+     * @param array $protoAuthor The author prototype.
+     * @return StatamicIdentity|
+     */
     private function makeAuthorFromArrayPrototype($protoAuthor)
     {
-        return null;
+        if (is_array($protoAuthor) == false) {
+            return null;
+        }
+
+        // If the author prototype contains the data attribute indicating that
+        // it was left by an authenticated user at some point, let's locate
+        // that user; if they exist, create the Author from that user.
+        if (array_key_exists(AuthorContract::AUTHENTICATED_USER_ID, $protoAuthor)) {
+            $potentialStatamicUser = $this->statamicUserProvider->retrieveById($protoAuthor[AuthorContract::AUTHENTICATED_USER_ID]);
+
+            if ($potentialStatamicUser !== null) {
+                $identity = $this->makeAuthorFromStatamicUser($potentialStatamicUser);
+
+                foreach ($protoAuthor as $key => $value) {
+                    $identity->setDataAttribute($key, $value);
+                }
+
+                return $identity;
+            }
+        }
+
+        $transientIdentity = new StatamicIdentity();
+
+        $transientIdentity->setId(null);
+        $transientIdentity->setIsTransient(true);
+
+        if (array_key_exists(AuthorContract::KEY_NAME, $protoAuthor)) {
+            $transientIdentity->setDisplayName($protoAuthor[AuthorContract::KEY_NAME]);
+        }
+
+        // Iterate all properties and set them on the identity context.
+        foreach ($protoAuthor as $key => $value) {
+            $transientIdentity->setDataAttribute($key, $value);
+        }
+
+        return $transientIdentity;
     }
 
     /**

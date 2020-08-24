133eaba713f83c718379afded2708ec60c30f30f
John Koster <john@stillat.com>
2020-08-23 22:01:43 -0500

    Implements a form handler to check if a comment is spam after submission

--- src/Guard/FormHandler.php
+++ src/Guard/FormHandler.php
@@ -3,10 +3,27 @@
 namespace Stillat\Meerkat\Guard;
 
 use Stillat\Meerkat\Core\Contracts\Comments\CommentContract;
+use Stillat\Meerkat\Core\Contracts\Storage\CommentStorageManagerContract;
 use Stillat\Meerkat\Core\Guard\SpamService;
+use Stillat\Meerkat\Core\GuardConfiguration;
 
+/**
+ * Class FormHandler
+ *
+ * Executed after a comment is saved or updated.
+ *
+ * @package Stillat\Meerkat\Guard
+ * @since 2.0.0
+ */
 class FormHandler
 {
+
+    /**
+     * The Meerkat Core guard configuration container.
+     *
+     * @var GuardConfiguration
+     */
+    protected $config = null;
 
     /**
      * The SpamService instance.
@@ -15,15 +32,40 @@ class FormHandler
      */
     protected $spamService = null;
 
-    public function __construct(SpamService $service)
+    /**
+     * The CommentStorageManagerContract implementation instance.
+     *
+     * @var CommentStorageManagerContract
+     */
+    protected $storageManager = null;
+
+    public function __construct(GuardConfiguration $config, SpamService $service, CommentStorageManagerContract $commentManager)
     {
+        $this->config = $config;
         $this->spamService = $service;
+        $this->storageManager = $commentManager;
     }
 
+    /**
+     * Checks the provided comment is spam or not.
+     *
+     * @param CommentContract $comment The comment to test.
+     */
     public function checkForSpam(CommentContract $comment)
     {
         $isSpam = $this->spamService->isSpam($comment);
-        dd('inside form handler', $comment, $isSpam, $this->spamService);
+
+        if ($this->spamService->hasErrors() === false) {
+            if ($isSpam === false) {
+                $this->storageManager->setIsHamById($comment->getId());
+            } else {
+                $this->storageManager->setIsSpamById($comment->getId());
+            }
+        } else {
+            if ($this->config->unpublishOnGuardFailures === true) {
+                $this->storageManager->setIsNotApprovedById($comment->getId());
+            }
+        }
     }
 
 }

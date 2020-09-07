e5d2ed7ee2935e8cf2066d8df4235e0d64b89ed8
John Koster <john@stillat.com>
2020-09-07 16:06:33 -0500

    Adds new display strings

--- resources/lang/en/display.php
+++ resources/lang/en/display.php
@@ -4,7 +4,12 @@ return [
 
     'per_page' => 'Per page',
     'comment_header' => 'Comments',
-    'loading' => 'Loading'
+    'loading' => 'Loading',
+
+    'header_author' => 'Author',
+    'header_comment' => 'Comment',
+
+    'author_in_reply_to' => 'In reply to',
 
 ];
 

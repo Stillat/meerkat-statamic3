161dbe6b807dbd85972deec709897908fbae7e61
John Koster <john@stillat.com>
2020-09-07 15:34:03 -0500

    General cleanup

--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -4,7 +4,6 @@ namespace Stillat\Meerkat\Http\Controllers;
 
 use Statamic\Http\Controllers\CP\CpController;
 use Stillat\Meerkat\Concerns\UsesTranslations;
-use Stillat\Meerkat\Core\Contracts\Data\DataSetContract;
 
 class DashboardController extends CpController
 {

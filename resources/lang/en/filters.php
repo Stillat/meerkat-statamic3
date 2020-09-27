21d0503d80967cfd1f4620c8b1a7d355b48654dd
John Koster <john@stillat.com>
2020-09-27 13:04:51 -0500

    Adds a secondary message to the no data display if there **is** data, but not for current filter

--- resources/lang/en/filters.php
+++ resources/lang/en/filters.php
@@ -10,6 +10,7 @@ return [
     'pending' => 'Pending',
 
     'no_data' => 'There are no comments to display',
+    'no_data_filter' => 'This is most likely due to current filters',
     'search_placeholder' => 'Search within comments...'
 
 ];

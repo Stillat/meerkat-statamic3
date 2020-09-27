71a1b1cac5919c5273f4d3a7e514fe129b1f2e5d
John Koster <john@stillat.com>
2020-09-27 08:35:08 -0500

    Refactors to POST request for URL limit

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Stillat\Meerkat\Addon;
 
 Route::group(['prefix' => Addon::getApiPrefix()], function () {
 
-    Route::get('/', '\Stillat\Meerkat\Http\Controllers\Api\IndexController@index');
+    Route::post('/', '\Stillat\Meerkat\Http\Controllers\Api\IndexController@index');
 
     Route::group(['prefix' => 'telemetry'], function () {
         Route::get('/', '\Stillat\Meerkat\Http\Controllers\Api\TelemetryController@index');
@@ -14,7 +14,7 @@ Route::group(['prefix' => Addon::getApiPrefix()], function () {
     });
 
     Route::group(['prefix' => 'comments'], function () {
-        Route::get('/', '\Stillat\Meerkat\Http\Controllers\Api\CommentsController@search');
+        Route::post('/search', '\Stillat\Meerkat\Http\Controllers\Api\CommentsController@search');
         Route::post('/update', '\Stillat\Meerkat\Http\Controllers\Api\UpdateCommentController@updateComment');
         Route::post('/reply', '\Stillat\Meerkat\Http\Controllers\Api\ReplyCommentController@reply');
         Route::post('/publish', '\Stillat\Meerkat\Http\Controllers\Api\PublishCommentController@publishComment');

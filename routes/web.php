0c7fe6995b73c4358c4436c329b4d3b5fb062b36
John Koster <john@stillat.com>
2020-10-07 17:45:20 -0500

    Removal of legacy stubs as part of clean up - will need to bring back later

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/!/Meerkat/socialize', '\Stillat\Meerkat\Http\Controllers\SocializeController@postSocialize');
 
-// /!/ Endpoints are just there for backwards compatibility.
-// TODO: don't use closures to allow for route optimization.
-// TODO: Re-enable.
-/*
-Route::post('/!/Meerkat/delete', function () {
-
-});
-
-Route::post('/!/Meerkat/update', function () {
-
-});
-
-
-Route::get('/!/Meerkat/test', function () {
-    return 'testing Legacy Route Structure.';
-});
-*/
 include_once 'api.php';
 include_once 'emit.php';

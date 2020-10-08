1a85bd1cafb73c681e6dad5d81f6cf8fef70c263
John Koster <john@stillat.com>
2020-10-07 21:10:43 -0500

    banned words list docs

--- config/wordlist.php
+++ config/wordlist.php
@@ -2,6 +2,15 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Banned Words
+    |--------------------------------------------------------------------------
+    |
+    | A list of words that is checked against each new comment. If a comment
+    | contains any of the words in this list, it will be marked as spam.
+    |
+    */
     'banned' => [
 
     ]

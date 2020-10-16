c8b845be3b7a074840af33dfbf1f5a121b8a63a6
John Koster <john@stillat.com>
2020-10-15 21:13:44 -0500

    Makes header use translation strings

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,7 @@
 @extends('statamic::layout')
-@section('title', 'Comments')
+@section('title', trans('meerkat::display.header_comments'))
 @section('wrapper_class', 'max-w-full')
 
 @section('content')
-
-
     <meerkat-comment-thread></meerkat-comment-thread>
 @stop

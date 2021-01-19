964e9dcca9ff80ac4eb7b2e06332336ad7e6b8e3
John Koster <john@stillat.com>
2021-01-19 06:41:55 -0600

    Version sync bump

--- src/Addon.php
+++ src/Addon.php
@@ -18,7 +18,7 @@ class Addon
     public const CODE_ADDON_NAME = 'meerkat';
     public const ROUTE_PREFIX = 'meerkat';
     public const ADDON_ID = 'stillat/meerkat';
-    public const VERSION = '2.1.22-beta3';
+    public const VERSION = '2.1.24-beta3';
 
     /**
      * Gets the addon API prefix.

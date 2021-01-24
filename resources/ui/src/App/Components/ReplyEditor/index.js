e5210f047b706cdaf9654ebe0d6b3590bc8fa34e
John Koster <john@stillat.com>
2021-01-24 07:17:54 -0600

    Implements #151

--- resources/ui/src/App/Components/ReplyEditor/index.js
+++ resources/ui/src/App/Components/ReplyEditor/index.js
@@ -1,19 +1,39 @@
 import template from './template.html';
 import UsesTranslator from '../../Mixins/usesTranslator';
+import ActionHandler from '../../Mixins/actionHandler';
+import CanDismissAction from '../../Mixins/canDismissAction';
+import LostChangesHandler from '../CommentEditor/Handlers/lostChangesHandler';
+import ActionState from '../../actionState';
 
 require('./style.less');
 
 export default {
   template: template,
-  mixins: [UsesTranslator],
+  mixins: [UsesTranslator, ActionHandler, CanDismissAction],
   props: {
     comment: {
       type: Object,
       default: null
     }
   },
+  data() {
+    return {
+      currentAction: null
+    };
+  },
   methods: {
     cancel() {
+      if (this.comment.state.reply.content.trim().length > 0) {
+        let handler = new LostChangesHandler(this.comment);
+
+        this.confirm(handler).onConfirm(function (state : ActionState) {
+          this.$emit('reply-canceled', this.comment);
+          this.comment.cancelReply();
+        }.bind(this));
+
+        return;
+      }
+
       this.$emit('reply-canceled', this.comment);
       this.comment.cancelReply();
     }

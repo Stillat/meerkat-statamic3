1dd07480b782012d12ddd84aec12604d229c274f
John Koster <john@stillat.com>
2021-02-06 08:45:37 -0600

    Adds some hints for databas table columns

--- src/Configuration/Drivers/Eloquent/Models/SupplementalPreferences.php
+++ src/Configuration/Drivers/Eloquent/Models/SupplementalPreferences.php
@@ -2,6 +2,7 @@
 
 namespace Stillat\Meerkat\Configuration\Drivers\Eloquent\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Concerns\HasTimestamps;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -13,6 +14,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  *
  * @package Stillat\Meerkat\Configuration\Drivers\Eloquent\Models
  * @since 2.3.0
+ * @property int id The storage system's identifier.
+ * @property string preferences The json_encoded'd preferences.
+ * @property Carbon created_at The date/time instance the thread was created.
+ * @property Carbon updated_at The date/time instance the thread was last updated.
+ * @property Carbon|null deleted_at The date/time instance the thread was deleted, if deleted.
  */
 class SupplementalPreferences extends Model
 {

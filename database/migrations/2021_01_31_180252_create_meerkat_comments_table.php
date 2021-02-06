1abe76aa8b71a71a03b71db924bf04fb82415158
John Koster <john@stillat.com>
2021-02-06 11:20:40 -0600

    Scaffolding/early design of comments table

---
+++ database/migrations/2021_01_31_180252_create_meerkat_comments_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMeerkatCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meerkat_comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('parent_compatibility_id')->nullable();
+            $table->bigInteger('compatibility_id');
+            $table->uuid('statamic_user_id')->nullable();
+            $table->uuid('thread_context_id');
+            $table->integer('depth');
+            $table->boolean('is_root');
+            $table->boolean('is_parent');
+            $table->boolean('is_published');
+            $table->boolean('is_spam')->nullable();
+            $table->longText('content');
+            $table->json('comment_attributes');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meerkat_comments');
+    }
+}
